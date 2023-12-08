c823d45a9a7f31df2db10555feee8be1d4d16aa5
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2023-12-08 18:35:05 -0300

    adding get + post

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -16,20 +16,23 @@ public class Medico {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
-
     private String email;
+    private String telefone;
     private String crm;
     @Enumerated(EnumType.STRING)
     private Especialidade especialidade;
 
     @Embedded
+    @Getter
     private Endereco endereco;
 
     public Medico(DadosCadastroMedico dados) {
         this.nome = dados.nome();
         this.email = dados.email();
+        this.telefone = dados.telefone();
         this.crm = dados.crm();
         this.endereco = new Endereco(dados.endereco());
         this.especialidade = dados.especialidade();
     }
+
 }
